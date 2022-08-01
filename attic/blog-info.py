690b889478e24ea07818cd3f38b6d835b78745e8
Ludvins <theludvins@gmail.com>
2022-08-01 14:35:29 +0200

    update

--- attic/blog-info.py
+++ attic/blog-info.py
@@ -12,37 +12,44 @@ import urllib.request
 from bs4 import BeautifulSoup
 import re
 
-import html.parser; h = html.parser.HTMLParser()
+import html.parser
+
+h = html.parser.HTMLParser()
+
 
 def getContent(url):
-  response = urllib.request.urlopen(url)
-  html = response.read().decode("utf8")
-  if not html: sys.exit(42)
-  return h.unescape(html)
+    response = urllib.request.urlopen(url)
+    html = response.read().decode("utf8")
+    if not html:
+        sys.exit(42)
+    return h.unescape(html)
+
 
 def numPostsFromArchives(archiveContent):
-  soup = BeautifulSoup(archiveContent)
-  mainCol = soup.find('div', attrs={'id': 'col1'})
-  return len(mainCol.find_all('li'))
+    soup = BeautifulSoup(archiveContent)
+    mainCol = soup.find("div", attrs={"id": "col1"})
+    return len(mainCol.find_all("li"))
+
 
 def parseTags(tagContent):
-  soup = BeautifulSoup(tagContent)
-  mainCol = soup.find('div', attrs={'id': 'col1'})
-  headers = mainCol.find_all('h3')
-  tags = {}
-  for header in headers:
-    tp = re.match("(\S*) - (\d*) Posts?", header.text)
-    tags[tp.group(1)] = int(tp.group(2))
+    soup = BeautifulSoup(tagContent)
+    mainCol = soup.find("div", attrs={"id": "col1"})
+    headers = mainCol.find_all("h3")
+    tags = {}
+    for header in headers:
+        tp = re.match("(\S*) - (\d*) Posts?", header.text)
+        tags[tp.group(1)] = int(tp.group(2))
 
-  sortedTags = sorted(tags.items(), key=operator.itemgetter(1), reverse=True)
-  return [x[0] for x in sortedTags]
+    sortedTags = sorted(tags.items(), key=operator.itemgetter(1), reverse=True)
+    return [x[0] for x in sortedTags]
 
-if __name__=='__main__':
-  archiveContent = getContent('http://bamos.github.io/archives/')
-  tagContent = getContent('http://bamos.github.io/tags/')
-  print(
-    "{} posts across the following tags, listed by highest frequency.".format(
-      numPostsFromArchives(archiveContent)
+
+if __name__ == "__main__":
+    archiveContent = getContent("http://ludvins.github.io/archives/")
+    tagContent = getContent("http://ludvins.github.io/tags/")
+    print(
+        "{} posts across the following tags, listed by highest frequency.".format(
+            numPostsFromArchives(archiveContent)
+        )
     )
-  )
-  print(", ".join(parseTags(tagContent)))
+    print(", ".join(parseTags(tagContent)))
--- attic/github-info.py
+++ attic/github-info.py
@@ -11,25 +11,24 @@ import json
 import argparse
 import sys
 
-def getRepos(user):
-  url = 'https://api.github.com/users/' + user + '/repos'
-  try:
-    request = Request(url)
-    handler = urlopen(request)
-    reposJson = json.loads(handler.read().decode('utf8'))
-    handler.close()
-    return reposJson
-  except:
-    print('Failed to get repos.')
-    sys.exit(-1)
 
-if __name__=="__main__":
-  repos = getRepos('bamos')
-  forkCount = len(list(filter(lambda r: r['fork'], repos)))
-  origCount = len(repos) - forkCount
-  print(
-    "{} original repositories, {} forked repositories.".format(
-      origCount,
-      forkCount
+def getRepos(user):
+    url = "https://api.github.com/users/" + user + "/repos"
+    try:
+        request = Request(url)
+        handler = urlopen(request)
+        reposJson = json.loads(handler.read().decode("utf8"))
+        handler.close()
+        return reposJson
+    except:
+        print("Failed to get repos.")
+        sys.exit(-1)
+
+
+if __name__ == "__main__":
+    repos = getRepos("ludvins")
+    forkCount = len(list(filter(lambda r: r["fork"], repos)))
+    origCount = len(repos) - forkCount
+    print(
+        "{} original repositories, {} forked repositories.".format(origCount, forkCount)
     )
-  )
